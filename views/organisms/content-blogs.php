03a7ed8997205a9b2d3330b596ec62bdb86f20cd
EduardoAC <eduardo@dreammakerfactory.com>
2015-09-18 15:11:32 +0100

    Implement content blog v1.0 - Add image picture - Add styles to fit across all the devices

--- views/organisms/content-blogs.php
+++ views/organisms/content-blogs.php
@@ -5,16 +5,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+use yii\helpers\Html;
+use yii\helpers\Url;
 
+$codeProjectsLink = Url::to(['pages/my-projects']);
 ?>
-<section class="clearfix">
-    <div class="col-md-4">
-        <img src="http://demo.patternlab.io/images/fpo_square.png" with="240" height="240"/>
+<section class="container clearfix content-blogs">
+    <div class="col-md-5">
+        <?= Html::img('images/ideas-content-blog.jpg', array('class'=>'img-responsive'));?>
     </div>
-    <div class="col-md-6">
-        <p>I'm passionate about technology, buisness and personal growing so I wanted to contribute to internet sharing</p>
-        <p>Therefore I added two areas in that will contribute with my own posts and ideas about it</p>
-        <p>These are <a>business and technology blog</a> and <a>the brainstorming forum</a></p>
-        <p>where I publish different about topic related with all the topics that I mentioned before</p>
+    <div class="col-md-7">
+        <p class="text">I'm passionate about technology, buisness and personal growing so I wanted to contribute to internet sharing</p>
+        <p class="text">Therefore I added two areas in that will contribute with my own posts and ideas about it</p>
+        <p class="text">These are <a>business and technology blog</a> and <a>the brainstorming forum</a></p>
+        <p class="text">where I publish different about topic related with all the topics that I mentioned before</p>
     </div>
 </section>
