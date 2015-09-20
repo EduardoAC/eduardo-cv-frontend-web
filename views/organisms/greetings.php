324a9d73a674be4c0b0bcce076cae66383b742f8
EduardoAC <eduardo@dreammakerfactory.com>
2015-09-20 12:29:30 +0100

    Implement greeting v1.0 - Add styles and image to represent this section - Add future page where I will store the project, how did i build it - Improve position images greeting and timeline - Hide for now how do you look yourself

--- views/organisms/greetings.php
+++ views/organisms/greetings.php
@@ -5,11 +5,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$MyInteractiveCVProjectLink = Url::to(['projects/how-did-i-build-my-interactive-cv']);
 ?>
-<section>
-    <p>Thanks for reading my interactive curriculum</p>
-    <p>I hope you like it.</p>
-    <p>Do you want to hire me? <a>contact me by email</a></p>
-    <p>Would you like to follow me or give me some feedback?</p>
-    <?php echo \Yii::$app->view->renderFile('@molecules/follow-and-social-icons.php');?>
+<section class="container clearfix greetings-contact">
+    <h2>Thanks for reading my interactive curriculum</h2>
+    <div class="col-md-5">
+        <?= Html::img('images/finding-the-secrets.jpg', array('class'=>'img-responsive'));?>
+    </div>
+    <div class="col-md-7">
+        <p class="text">I hope you like it. Discover more in each page some of them have amazing secrets. As my frontend career is done in Angular and NodeJs</p>
+        <p class="text">Are you ready to discover all of them? Then go to <a href="<?=$MyInteractiveCVProjectLink?>">how did i build my intereactive CV</a></p>
+        <p class="text">Do you want to hire me or give me some feedback? You can <a href="mailto:eduardo@dreammakerfactory.com">contact me by email</a></p>
+        <?php echo \Yii::$app->view->renderFile('@molecules/follow-and-social-icons.php');?>
+    </div>
 </section>
