c473c56bc14109dbf5b18c29d68088e60c10b4bf
EduardoAC <eduardo@dreammakerfactory.com>
2016-01-22 12:36:23 +0000

    Fix issues with name picture and error typing

--- views/organisms/introduction.php
+++ views/organisms/introduction.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
     <?= Html::img('images/introduction-image-1280.jpg',array('class'=>'img-responsive'));?>
     <div class="title-block">
         <h1>
-            <p>Welcolme to my interactive curriculum</p>
+            <p>Welcome to my interactive curriculum</p>
             <p>My name is Eduardo Aparicio Cardenes</p>
         </h1>
         <p class="hidden-xs">One place that define my worker soul and share with you</p>
