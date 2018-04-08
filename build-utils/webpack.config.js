0d5fd704182e8a469fb9738069fcd5ba5c5dc284
Eduardo Aparicio Cardenes <eduardo.aparicio.cardenes@gmail.com>
2018-04-08 11:57:31 +0200

    Setup static analysis

---
+++ build-utils/webpack.config.js
@@ -0,0 +1,19 @@
+modules.exports = {
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: [
+          "babel-loader",
+        ],
+      }
+    ]
+  }
+};
