c39c927c75e500f3327893d04bd7f6865febf9e4
Eduardo <EduardoAC@users.noreply.github.com>
2024-12-16 07:46:34 +0000

    Update

--- views/pages/my-projects.php
+++ views/pages/my-projects.php
@@ -20,15 +20,15 @@ MyprojectspageAsset::register($this);
     <h1 class='container'>My projects</h1>
     <section class="container introduction">
         <p>
-           I started to have ideas and dream since I was child however 
-           it wasn't until I joined as a Web Developer for a wonderful company call 
-           <a href='http://www.globaincubator.com' target="_blank" rel="nofollow">GlobalIncubator</a>
-           that I realise all the ideas and opportinities that exist 
-           in the world to make it better and succesful business. 
-           There I met one of the most amazing people I could imagine in a 
-           atmosphere of innovation and hard work with people as Pablo Trianfilo, 
-           Luis Gonzalez-Blanch, Jose Sanz Polo and many others. 
-        </p>            
+           I started to have ideas and dream since I was child however
+           it wasn't until I joined as a Web Developer for a wonderful company call
+           <a href='http://www.globaincubator.com' target="_blank" rel="nofollow">&nbsp;GlobalIncubator&nbsp;</a>&nbsp;
+           that I realise all the ideas and opportinities that exist
+           in the world to make it better and succesful business.
+           There I met one of the most amazing people I could imagine in a
+           atmosphere of innovation and hard work with people as Pablo Trianfilo,
+           Luis Gonzalez-Blanch, Jose Sanz Polo and many others.
+        </p>
         <p>
            During over two years I learned a lot about Innovation, Ideas and how to make it happens but
            more important that you will never achieve anything if you don't take action.
@@ -42,8 +42,8 @@ MyprojectspageAsset::register($this);
         </p>
     </section>
     <?php echo \Yii::$app->view->renderFile('@organisms/color-legend-projects.php',[]);?>
-    <section class="collection-projects">    
-        <?php 
+    <section class="collection-projects">
+        <?php
             foreach($projects as $data):
                 echo \Yii::$app->view->renderFile('@molecules/project-description-block.php',$data);
             endforeach;
@@ -53,21 +53,21 @@ MyprojectspageAsset::register($this);
         <h2>Final Considerations</h2>
         <p>
             All these projects you can see above are only a part of the content.
-            Currently, I've been in several hackathons and event accross 3 
+            Currently, I've been in several hackathons and event accross 3
             countries and two continents. I develop dozens of ideas and implemented
-            5 of them.            
+            5 of them.
         </p>
         <p>
             However, I admit it's take time to put all together after a decade of work
-            and dedication from my spare time in a unique place but no worries, 
+            and dedication from my spare time in a unique place but no worries,
             I will be happy to talk about anytime and I will be adding content slowly
             but constant because I love it.
         </p>
         <p>
             Often I hear about people to tell me that I must start my own business
             and I will but currently I love to help another people to achieve them goal
-            learning a lot of things from them. I guess I want to learn from you 
-            if you give me the chance.          
+            learning a lot of things from them. I guess I want to learn from you
+            if you give me the chance.
         </p>
     </section>
 </article>
