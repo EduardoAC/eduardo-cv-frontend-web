273b696431debd12401a269faa84d8e2f4f0c916
Eduardo AC <EduardoAC@users.noreply.github.com>
2025-07-20 07:22:36 +0100

    Clean orphan code

--- app/projects/how-do-i-build-it/page.tsx
+++ app/projects/how-do-i-build-it/page.tsx
@@ -34,7 +34,7 @@ export default function HowDoIBuildItPage() {
         />
       </div>
       <h1>Coming soon</h1>
-      <p>I'm working on this section. Please come back later.</p>
+      <p className='text-center'>I'm working on this section. Please come back later.</p>
     </section>
   )
 } 
